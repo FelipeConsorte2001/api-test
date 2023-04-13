224390e406f7baa49ddae82c977189474beb5d50
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2023-04-13 20:01:23 -0300

    feat list and created users

--- src/app.js
+++ src/app.js
@@ -3,4 +3,13 @@ const app = require('express')();
 app.get('/', (req, res) => {
   res.status(200).send();
 });
+app.get('/users', (req, res) => {
+  const users = [
+    { name: 'John Doe', mail: 'joen#mail.com' },
+  ];
+  res.status(200).json(users);
+});
+app.post('/users', (req, res) => {
+  return res.status(201).json({ name: 'Walter Mitty', mail: 'joen#mail.com' });
+});
 module.exports = app;
