d1feb34cf9f0d22734a2117cadda0b320b2f077e
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2023-04-14 19:54:17 -0300

    feat config middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const app = require('express')();
 
+const consign = require('consign');
+
+consign({ cwd: 'src', verbose: false }).include('./config/middlewares.js').into(app);
 app.get('/', (req, res) => {
   res.status(200).send();
 });
@@ -10,6 +13,6 @@ app.get('/users', (req, res) => {
   res.status(200).json(users);
 });
 app.post('/users', (req, res) => {
-  return res.status(201).json({ name: 'Walter Mitty', mail: 'joen#mail.com' });
+  res.status(201).json(req.body);
 });
 module.exports = app;
