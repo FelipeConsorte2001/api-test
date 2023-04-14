0689b06de16ec83b174a89de8f5fc93752ebfc2f
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2023-04-14 20:17:16 -0300

    feat config router

--- src/app.js
+++ src/app.js
@@ -2,17 +2,13 @@ const app = require('express')();
 
 const consign = require('consign');
 
-consign({ cwd: 'src', verbose: false }).include('./config/middlewares.js').into(app);
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .then('./routes')
+  .then('./config/routes.js')
+  .into(app);
 app.get('/', (req, res) => {
   res.status(200).send();
 });
-app.get('/users', (req, res) => {
-  const users = [
-    { name: 'John Doe', mail: 'joen#mail.com' },
-  ];
-  res.status(200).json(users);
-});
-app.post('/users', (req, res) => {
-  res.status(201).json(req.body);
-});
+
 module.exports = app;
