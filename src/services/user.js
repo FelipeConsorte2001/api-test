1a51d468a843335804bd57b00ee7f0f7b3a087d9
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2023-04-22 14:34:45 -0300

    fix: translating text into English

--- src/services/user.js
+++ src/services/user.js
@@ -3,11 +3,11 @@ module.exports = (app) => {
     return app.db('users').where(filter).select();
   };
   const save = async (user) => {
-    if (!user.name) return { error: 'Nome é um atributo obrigatorio' };
-    if (!user.mail) return { error: 'Email é um atributo obrigatorio' };
-    if (!user.passwd) return { error: 'Senha é um atributo obrigatorio' };
+    if (!user.name) return { error: 'Name is a mandatory attribute' };
+    if (!user.mail) return { error: 'Email is a mandatory attribute' };
+    if (!user.passwd) return { error: 'Password is a mandatory attribute' };
     const userDb = await findAll({ mail: user.mail });
-    if (userDb && userDb.length > 0) return { error: 'Já existe um usuario com esse email' };
+    if (userDb && userDb.length > 0) return { error: 'There is already a user with this email address' };
     return app.db('users').insert(user, '*');
   };
   return { findAll, save };
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -3,39 +3,39 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 const mail = `${Date.now()}@gmail.com`;
-test('Deve listar todos os usários', () => {
+test('Must list all users', () => {
   return request(app).get('/users').then((res) => {
     expect(res.status).toBe(200);
     expect(res.body.length).toBeGreaterThan(0);
   });
 });
-test('Deve inserir usuário com sucesso', () => {
+test('Must insert user successfully', () => {
   return request(app).post('/users').send({ name: 'felipearaujo3', mail, passwd: 'felipearaujo3' }).then((res) => {
     expect(res.status).toBe(201);
     expect(res.body.name).toBe('felipearaujo3');
   });
 });
-test('Não deve inserir um usuario sem nome', () => {
+test('You must not enter a user without a name', () => {
   return request(app).post('/users').send({ mail, passwd: '123456' }).then((res) => {
     expect(res.status).toBe(400);
-    expect(res.body.error).toBe('Nome é um atributo obrigatorio');
+    expect(res.body.error).toBe('Name is a mandatory attribute');
   });
 });
-test('Não deve inserir um usuario sem email', async () => {
+test('You must not enter a user without email', async () => {
   const result = await request(app).post('/users').send({ name: 'felipearaujo3', passwd: 'felipearaujo3' });
   expect(result.status).toBe(400);
-  expect(result.body.error).toBe('Email é um atributo obrigatorio');
+  expect(result.body.error).toBe('Email is a mandatory attribute');
 });
-test('Não deve inserir um usuario sem senha', (done) => {
+test('You must not enter a user without a password', (done) => {
   request(app).post('/users').send({ name: 'felipearaujo3', mail: 'felipearaujo@gmail.com' }).then((res) => {
     expect(res.status).toBe(400);
-    expect(res.body.error).toBe('Senha é um atributo obrigatorio');
+    expect(res.body.error).toBe('Password is a mandatory attribute');
     done();
   });
 });
-test('Não deve inserir um usuario com email existente', () => {
+test('You must not enter a user with an existing email address', () => {
   return request(app).post('/users').send({ name: 'felipearaujo3', mail, passwd: 'felipearaujo3' }).then((res) => {
     expect(res.status).toBe(400);
-    expect(res.body.error).toBe('Já existe um usuario com esse email');
+    expect(res.body.error).toBe('There is already a user with this email address');
   });
 });
